8428e61d7da89f926c6688350bcbbf38c85a1a8a
asumpta <sumgathoni@gmail.com>
2023-03-31 12:52:59 +0300

    add array button and listener

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # change-background-colour
 1 of 52 JavaScript beginner projects
+
+Functionality
+Change the background color of a div on the click of a button.
---
+++ index.js
@@ -0,0 +1,10 @@
+//colors array
+const colors = ['plum','black','yellow','pink','magenta','blue','powder blue','orange'];
+
+//get button
+const button = document.getElementById('button');
+
+//add listener
+button.addEventListener('click', ()=> {
+    console.log('test');
+})
